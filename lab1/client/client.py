25b47faea38a59b039aff414d3d2e7e2c315f46f
Timofey <nikiforov.ta@edu.spbstu.ru>
2021-09-28 02:11:31 +0300

    *_*

--- lab1/client/client.py
+++ lab1/client/client.py
@@ -14,8 +14,10 @@ class Client:
     def __init__(self):
         try:
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_socket.connect(('192.168.152.249', 6666))
+            self.client_socket.connect(('localhost', 6666))
             self.nickname = ""
+            self.set_nickname()
+            self.client_socket.send(f"{{'requestType':'greeting', 'message':'{self.nickname}'}}\r\n".encode('utf-8'))
 
         except TimeoutError:
             print("Сервер не отвечал некоторое время")
@@ -28,12 +30,19 @@ class Client:
         while True:
             try:
                 message = ast.literal_eval(self.client_socket.recv(1024).decode('utf-8'))
-                print(f"\n[{message['time']}] {message['message']}")
-                if message['requestType'] == 'greeting':
-                    self.set_nickname()
-                    self.client_socket.send(
-                        f"{{'responseType':'greeting', 'message':'{self.nickname}'}}\r\n".encode('utf-8'))
-                if message['requestType'] == 'exit':
+                # if message['responseType'] == 'greetingError':
+                #     self.set_nickname()
+                #     self.client_socket.send(
+                #         f"{{'requestType':'greeting', 'message':'{self.nickname}'}}\r\n".encode('utf-8'))
+                if message['requestType'] in ['info', 'message']:
+                    print(f"\n[{message['time']}] {message['nickname']} {message['message']}")
+                    if 'file' in message.keys():
+                        filename = message_data.save_file(message['file'], message['file_extension'])
+                        print(f"Received {filename} from {message['nickname']}")
+                elif message['responseType'] == 'exception':
+                    if message['message'] == '0':  # TODO
+                        pass
+                elif message['responseType'] == 'exit':
                     raise ServerClosed
 
                 # {'requestType':'greeting', 'message':'Добро пожаловать в чат! Введите пожалуйста свой никнейм: ', 'time':'Mon Sep 27 20:26:04 MSK 2021'}
@@ -74,21 +83,27 @@ class Client:
                     while not attached:
                         fp = input("Relative filepath:")
                         if fp == '':
-                            encoded_message = message_data.encode_message("client message without file", self.nickname,
-                                                                          message)
+                            self.client_socket.send(f"{{'requestType':'message', 'message':'{message}', "
+                                                    f"'file_extension':'', 'file':''}}".encode('utf-8'))
+
+                            # encoded_message = message_data.encode_message("client message without file", self.nickname,
+                            #                                               message)
                             break
                         try:
-                            encoded_message = message_data.encode_message("client message with file", self.nickname,
-                                                                          message, fp)
+                            ext, file = message_data.load_file(fp)
+                            self.client_socket.send(f"{{'requestType': 'message', 'message': '{message}', "
+                                                    f"'file_extension':'{ext}', 'file':'{file}'}}".encode('utf-8'))
+                            # encoded_message = message_data.encode_message("client message with file", self.nickname,
+                            #                                               message, fp)
                             attached = True
                         except FileNotFoundError:
-                            print(f"File not found in {fp}")
+                            print(f"File {fp} not found")
                             pass
                         except:
                             print()
                             print(traceback.format_exc())
 
-                    self.client_socket.send(encoded_message)
+                    # self.client_socket.send(encoded_message)
                 except Exception:
                     print("Сервер недоступен")
                     print(traceback.format_exc())
--- lab1/client/message_data.py
+++ lab1/client/message_data.py
@@ -1,17 +1,24 @@
 import pickle
-
-import matplotlib.pyplot as plt
+import time
 
 
 def encode_message(message_type, sender, text, fp=None):
     message = {"type": message_type, "sender": sender, "text": text}
     if message_type == "client message with file" and fp is not None:
-        file = open(fp, 'r')
-        data = plt.imread(file)
-        message['filename'] = file
-        message['attachment'] = data.encode('base64')
+        file = open(fp, 'rb')
+        message['file_extension'] = '.' + fp.split('.')[-1]
+        message['file'] = file
         file.close()
-    return text.encode('utf-8')
+    return message
+
+
+def load_file(fp):  # TODO
+    res = []
+    file = open(fp, 'rb')
+    res.append = '.' + fp.split('.')[-1]
+    res.append = file
+    file.close()
+    return res
 
 
 def decode_message(message):
@@ -26,3 +33,11 @@ def decode_message(message):
         return message['type'], message['sender'], message['time'], message['text'], message['filename']
     else:
         return message['type']
+
+
+def save_file(nickname, file, extension):  # TODO
+    filename = open(
+        f"./Downloads/{time.strftime('%d%M%Y%H%m', time.localtime())} attachment by {nickname}{extension}", 'wb')
+    filename.write(file)
+    filename.close()
+    return filename
