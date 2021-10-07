15cdd612188ba705f1e051b091a65a3c17aebdb3
Timofey <nikiforov.ta@edu.spbstu.ru>
2021-10-07 19:36:35 +0300

    added ignoring for decoding errors

--- lab1/client/client.py
+++ lab1/client/client.py
@@ -37,20 +37,23 @@ class Client:
                 while letter != b'\n':
                     message += letter
                     letter = self.client_socket.recv(1)
-                message = message.decode('utf-8')
+                message = message.decode('utf-8', 'ignore')
                 message = ast.literal_eval(message)
                 if message['parcelType'] == 'exit':
                     print(
-                        Fore.YELLOW + Back.BLACK + f"\n[{message_data.time_format(message['time'])}] {message['username']} покинул чат")
+                        Fore.YELLOW + Back.BLACK + f"\n[{message_data.time_format(message['time'])}] "
+                                                   f"{message['username']} покинул чат")
                 elif message['parcelType'] == 'greeting':
                     print(
-                        Fore.GREEN + Back.BLACK + f"\n[{message_data.time_format(message['time'])}] {message['username']} вошел в чат")
+                        Fore.GREEN + Back.BLACK + f"\n[{message_data.time_format(message['time'])}] "
+                                                  f"{message['username']} вошел в чат")
                     if message['username'] == self.username:
                         self.logged = True
                         print(Back.BLACK + Fore.GREEN + "Если тебе надоест общение, напиши !exit\n")
                 elif message['parcelType'] == 'message':
                     print(
-                        Fore.GREEN + Back.BLACK + f"\n[{message_data.time_format(message['time'])}] {message['username']} сказал: {message['message']}")
+                        Fore.GREEN + Back.BLACK + f"\n[{message_data.time_format(message['time'])}] "
+                                                  f"{message['username']} сказал: {message['message']}")
                     if message['attachmentSize'] != '0':
                         attachment = b''
                         self.client_socket.recv(1024)
@@ -58,13 +61,15 @@ class Client:
                         message_data.save_file(message['username'], message['attachmentType'],
                                                message['attachmentName'], attachment)
                         print(
-                            Fore.BLUE + Back.YELLOW + f"Received {message['attachmentName']} from {message['username']}")
+                            Fore.BLUE + Back.YELLOW + f"Received {message['attachmentName']} "
+                                                      f"from {message['username']}")
                 elif message['parcelType'] == 'exception':
                     if message['message'] == '1':  # TODO
                         print(Fore.RED + Back.BLACK + 'Имя пользователя уже занято!\n')
                         self.set_username(Back.BLACK + Fore.GREEN + "Username:")
                         self.client_socket.send(
-                            f"{{'parcelType':'greeting', 'message':'', 'username':'{self.username}', 'attachmentType':'', "
+                            f"{{'parcelType':'greeting', 'message':'', 'username':'{self.username}', "
+                            f"'attachmentType':'', "
                             f"'attachmentName':'', 'attachmentSize':'0'}}\r\n".encode('utf-8'))
                     else:
                         print(Fore.RED + Back.BLACK + 'Неизвестная ошибка сервера')
@@ -92,7 +97,8 @@ class Client:
                         fp = input(Fore.GREEN + Back.BLACK + "Relative filepath\n")
                         if message == "!exit":
                             self.client_socket.send(
-                                f"{{'parcelType':'exit', 'message':'', 'username':'{self.username}', 'attachmentType':'', "
+                                f"{{'parcelType':'exit', 'message':'', 'username':'{self.username}', "
+                                f"'attachmentType':'', "
                                 f"'attachmentName':'', 'attachmentSize':'0'}}\r\n".encode('utf-8'))
                             self.client_socket.close()
                             self.logged = False
