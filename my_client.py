0f5c6ac1c7289c15e87fffd2e68cb049fed31067
Fire <edvard.dunijsk@mail.ru>
2021-10-16 11:19:37 +0300

    первая лабораторная, все работает

---
+++ my_client.py
@@ -0,0 +1,68 @@
+import os
+import socket
+import threading
+from tzlocal import get_localzone
+
+code_table = 'utf-8'
+length_of_message = 8
+name = input("Username. No more then 16 symbols: ")
+if len(name) > 16:
+    print("No more then 16 symbols, try again")
+    nickname = input("Username: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+
+def receive_message():
+    buffer = 0
+    while True:
+        if buffer == 0:
+            message = client.recv(length_of_message).decode(code_table)
+            if message.strip().isdigit():
+                buffer = int(message.strip())
+            if message.strip() == "file":
+                file_header_len = int(client.recv(length_of_message).decode())
+                file_header = client.recv(file_header_len).decode(code_table) #заголовок это имя файла и размер файла
+                file_name = file_header[:file_header.find("<>")]
+                file_size = file_header[file_header.find("<>")+2:]
+                file_name = os.path.basename(file_name) # если имя файла задано как путь к нему, останется только имя файла
+                file_size = int(file_size)
+                f = open("send"+file_name, "wb")
+                file_data_write = client.recv(file_size)
+                f.write(file_data_write)
+                f.close()
+                print(f'File {file_name} is received ')
+        else:
+            message = client.recv(buffer).decode(code_table)
+            print(message)
+            buffer = 0
+
+
+def send_message():
+    while True:
+        local_tz = get_localzone()
+        message = input("")
+        if message == "file":
+            file_name = input("Type file name: ")
+            file_size = os.path.getsize(file_name)
+            client.send(f"{message}".encode(code_table))
+            file_name_size = f"{file_name}<>{file_size}".encode()
+            client.send(f"{len(file_name_size):<{length_of_message}}".encode(code_table))
+            client.send(file_name_size)
+            f = open(file_name, "rb")
+            file_data_write = f.read(file_size)
+            client.sendall(file_data_write)
+            f.close()
+            print(f'File {file_name} is send')
+        else:
+            message_send = f'<{local_tz}>{name}->{message}'.encode(code_table)
+            message_len_send = f'{len(message_send):<{length_of_message}}'.encode(code_table)
+            client.send(message_len_send + message_send)
+
+
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+
+write_thread = threading.Thread(target=send_message)
+write_thread.start()
---
+++ my_server.py
@@ -0,0 +1,68 @@
+import socket
+import threading
+from datetime import datetime
+from pytz import timezone
+
+
+code_table = 'utf-8'
+length_of_message = 8
+
+
+host = '127.0.0.1'
+port = 55555
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+u_sockets = []
+
+
+def broadcast(message):
+    for client in u_sockets:
+        client.send(message)
+
+
+def handle(client):
+    buffer = 0
+    while True:
+        try:
+            if buffer == 0:
+                message = client.recv(length_of_message).decode(code_table)
+                if message.strip().isdigit():   # если число, то это просто сообщение и нужно изменить длину буфера
+                    buffer = int(message.strip())
+                if message == "file":   # если file, то хотят отправить файл
+                    file_header_len = int(client.recv(length_of_message).decode())
+                    file_header = client.recv(file_header_len).decode(code_table)# заголовок это имя файла и размер файла
+                    file_size = file_header[file_header.find("<>")+2:]
+                    file_size = int(file_size)
+                    file_data_read = client.recv(file_size)
+                    broadcast(f'{"file":<{length_of_message}}'.encode(code_table) +
+                              f'{len(file_header.encode(code_table)):<{length_of_message}}'.encode(code_table) +
+                              file_header.encode(code_table) + file_data_read)
+            else:
+                message = client.recv(buffer).decode(code_table)
+                time_zone = message[message.find('<') + 1: message.find('>')]
+                now_time = datetime.now(timezone(time_zone)).strftime("%Y-%m-%d %H:%M") # время сервера измененное в соответствии с tz пользователя
+                message_send = '<' + now_time + '> ' + message[message.find('>') + 1:]
+                message_len_send = f'{len(message_send.encode(code_table)):<{length_of_message}}'.encode(code_table)
+                broadcast(message_len_send + message_send.encode(code_table))
+                print(message_send)
+                buffer = 0
+        except:
+            u_sockets.remove(client)
+            client.close()
+            break
+
+
+def receive_connection():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+        u_sockets.append(client)
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+receive_connection()
