5ac4c5feab88f4f3abe280e60ce39ed8be9ddaef
NevostruevaYana <newyana@mail.ru>
2021-10-27 23:53:39 +0400

    sending a request from a client

--- lab2/src/main/kotlin/com/monkeys/ntp/Utils.kt
+++ lab2/src/main/kotlin/com/monkeys/ntp/Utils.kt
@@ -3,9 +3,17 @@ package com.monkeys.ntp
 import com.monkeys.ntp.models.WorkType
 import com.monkeys.ntp.models.WorkType.*
 
-const val STANDARD_PORT = 8081
-
 const val DOWNLOADS_DIR = "PCSS downloads/"
+
+const val NTP_PORT = 123
+const val NTP_PACKET_SIZE = 48
+const val NTP_MODE_CLIENT = 3
+const val NTP_MODE_SERVER = 4
+const val NTP_VERSION = 3
+const val TRANSMIT_OFFSET = 40
+
+//70 years + 17 days (1904, 1908, ..., 1968) to sec
+const val OFFSET_1900_TO_1970 = (365L * 70L + 17L) * 24L * 60L * 60L
 
 fun parseArguments(args: List<String>): WorkType = when {
     args.isEmpty() -> {
@@ -36,8 +44,24 @@ fun printHelp() {
 }
 
 
-fun getFixedLengthString(dataSize: Int): String {
-    return dataSize.toString().padStart(8, '0')
+fun writeTimeStamp(buffer: ByteArray, time: Long) {
+    var offset = 0
+    var seconds = time / 1000L
+    val milliseconds = time % 1000L
+
+    seconds += OFFSET_1900_TO_1970
+
+    buffer[offset++] = (seconds shr 24).toByte()
+    buffer[offset++] = (seconds shr 16).toByte()
+    buffer[offset++] = (seconds shr 8).toByte()
+    buffer[offset++] = seconds.toByte()
+
+    val fraction = milliseconds * 0x100000000L / 1000L
+
+    buffer[offset++] = (fraction shr 24).toByte()
+    buffer[offset++] = (fraction shr 16).toByte()
+    buffer[offset++] = (fraction shr 8).toByte()
+    buffer[offset] = fraction.toByte()
 }
 
 
--- lab2/src/main/kotlin/com/monkeys/ntp/client/Client.kt
+++ lab2/src/main/kotlin/com/monkeys/ntp/client/Client.kt
@@ -1,9 +1,61 @@
 package com.monkeys.ntp.client
 
+import com.monkeys.ntp.*
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
 class Client(val host: String, val port: Int) {
 
     fun start() {
-        println("Client started!!!")
+        val socket = DatagramSocket()
+        socket.use {
+            println("Client started!!!")
+
+            val ntpPacket = ByteArray(NTP_PACKET_SIZE)
+            val clientTime = ByteArray(8)
+
+            val packet = DatagramPacket(ntpPacket, ntpPacket.size, InetAddress.getByName("pool.ntp.org"), NTP_PORT)
+            val response = DatagramPacket(ntpPacket, ntpPacket.size)
+
+            ntpPacket[0] = (NTP_VERSION.shl(3)).or(NTP_MODE_CLIENT).toByte()
+
+            val requestTime = System.currentTimeMillis()
+            writeTimeStamp(clientTime, requestTime)
+            System.arraycopy(clientTime,0,ntpPacket, TRANSMIT_OFFSET, 8)
+
+            //for read response
+            val requestTicks: Long = System.nanoTime() / 1000000L
+
+            socket.send(packet)
+
+            socket.receive(response);
+            response.data.forEach { println(it) }
+        }
+
     }
+
+
+//    private fun convertToLong(buffer: ByteArray, offset: Int): Long {
+//        val b0 = buffer[offset].toInt()
+//        val b1 = buffer[offset + 1].toInt()
+//        val b2 = buffer[offset + 2].toInt()
+//        val b3 = buffer[offset + 3].toInt()
+//
+//        // convert signed bytes to unsigned
+//        val i0 = if ((b0 and 0x80) == 0x80) (b0 and 0x7F) + 0x80 else b0
+//        val i1 = if (b1 and 0x80 == 0x80) (b1 and 0x7F) + 0x80 else b1
+//        val i2 = if (b2 and 0x80 == 0x80) (b2 and 0x7F) + 0x80 else b2
+//        val i3 = if (b3 and 0x80 == 0x80) (b3 and 0x7F) + 0x80 else b3
+//        return (i0.toLong() shl 24) + (i1.toLong() shl 16) + (i2.toLong() shl 8) + i3.toLong()
+//    }
+//
+//
+//    fun readTimeStamp(buffer: ByteArray, offset: Int): Long {
+//        val seconds = convertToLong(buffer, offset)
+//        val fraction = convertToLong(buffer, offset + 4)
+//        return (seconds - OFFSET_1900_TO_1970) * 1000 + fraction * 1000L / 0x100000000L
+//    }
+
 
 }
