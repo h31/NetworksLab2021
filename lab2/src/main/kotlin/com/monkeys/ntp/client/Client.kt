357a3f306b9fa648afbac3a27be4d16e733933ff
NevostruevaYana <newyana@mail.ru>
2021-11-03 17:31:18 +0400

    print time

--- lab2/src/main/kotlin/com/monkeys/ntp/client/Client.kt
+++ lab2/src/main/kotlin/com/monkeys/ntp/client/Client.kt
@@ -5,6 +5,7 @@ import com.monkeys.ntp.models.Header
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.util.*
 
 class Client(val host: String, val port: Int) {
 
@@ -17,6 +18,7 @@ class Client(val host: String, val port: Int) {
             val clientTime = ByteArray(8)
 
             val packet = DatagramPacket(ntpPacket, ntpPacket.size, InetAddress.getByName("localhost"), 4445)
+            //val packet = DatagramPacket(ntpPacket, ntpPacket.size, InetAddress.getByName("pool.ntp.org"), NTP_PORT)
             val response = DatagramPacket(ntpPacket, ntpPacket.size)
 
             ntpPacket[0] = (NTP_VERSION.shl(3)).or(NTP_MODE_CLIENT).toByte()
@@ -38,31 +40,10 @@ class Client(val host: String, val port: Int) {
             val h = Header(responseArray)
             checkValidServerResponse(h.leapIndicator, h.versionNumber, h.mode, h.stratum)
             h.printHeader()
+
+            println(Date(h.transmit))
         }
 
     }
-
-
-//    private fun convertToLong(buffer: ByteArray, offset: Int): Long {
-//        val b0 = buffer[offset].toInt()
-//        val b1 = buffer[offset + 1].toInt()
-//        val b2 = buffer[offset + 2].toInt()
-//        val b3 = buffer[offset + 3].toInt()
-//
-//        // convert signed bytes to unsigned
-//        val i0 = if ((b0 and 0x80) == 0x80) (b0 and 0x7F) + 0x80 else b0
-//        val i1 = if (b1 and 0x80 == 0x80) (b1 and 0x7F) + 0x80 else b1
-//        val i2 = if (b2 and 0x80 == 0x80) (b2 and 0x7F) + 0x80 else b2
-//        val i3 = if (b3 and 0x80 == 0x80) (b3 and 0x7F) + 0x80 else b3
-//        return (i0.toLong() shl 24) + (i1.toLong() shl 16) + (i2.toLong() shl 8) + i3.toLong()
-//    }
-//
-//
-//    fun readTimeStamp(buffer: ByteArray, offset: Int): Long {
-//        val seconds = convertToLong(buffer, offset)
-//        val fraction = convertToLong(buffer, offset + 4)
-//        return (seconds - OFFSET_1900_TO_1970) * 1000 + fraction * 1000L / 0x100000000L
-//    }
-
 
 }
