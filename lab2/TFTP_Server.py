2838545b9fae65f4d03ef5b68289ba4a74a90542
unknown <xboooox123@gmail.com>
2021-11-06 13:53:01 +0300

    lab2

--- lab2/TFTP_Server.py
+++ lab2/TFTP_Server.py
@@ -4,28 +4,12 @@ from ClientHandler import ClientHandler
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", 1069))
 
-clients = {} # {ip1 : {port1 : clientHandler1, port2 : clientHandler2}, ip2 : {port3 : clientHandler3}}
 while True:
 	try:
 		data, address = sock.recvfrom(1024)
 	except socket.error:
-		clients[address[0]][address[1]] = None
-		continue
+		break
 	
-	ip = address[0]
-	port = address[1]
-	new_client = False
-	if ip not in clients:
-		new_client = True
-		clients[ip] = {port : ClientHandler(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), address)}
-	else:
-		if port not in clients[ip]:
-			new_client = True
-		else:
-			if clients[ip][port] == None:
-				new_client = True
-		if new_client:
-			clients[ip][port] = ClientHandler(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), address)
-	
-	clients[ip][port].new_package(data)
-	if new_client: clients[ip][port].start()
+	handler = ClientHandler(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), address)
+	handler.new_package(data)
+	handler.start()
