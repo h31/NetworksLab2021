185ab2ee105dce10f4b7f0bc963cf0e4bd3e61ef
Bình Phạm <binhpttmdc@gmail.com>
2021-12-03 20:28:32 +0300

    lab3

---
+++ Client2/client.py
@@ -0,0 +1,113 @@
+#coding=utf-8
+import threading
+import socket
+from datetime import datetime
+import time
+import signal
+import os
+import re
+
+HEADER_LENGTH = 10
+SEPARATOR = "<SEPARATOR>"
+SEND_FILE = "SEND_FILE"
+
+IP = "127.0.0.1" #"networkslab-ivt.ftp.sh"
+PORT = 10000
+UID = "c97ec0d1-df22-41f4-858f-7beee9e1bbc4".encode("utf-8")
+CONNECT = "CONNECT"
+DISCONNECT = "DISCONNECT"
+
+
+def main():
+    nickname = input("Write name for chat: ")
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((IP, PORT))
+
+    def catch_interrupt(signal, frame):
+        clientsocket.shutdown(socket.SHUT_WR)
+        clientsocket.close()
+        os._exit(0)
+
+    signal.signal(signal.SIGINT, catch_interrupt)
+    nickname_code = nickname.encode('utf-8')
+    nickname_header = f"{len(nickname_code):<{HEADER_LENGTH}}".encode('utf-8')
+    clientsocket.send(nickname_header + nickname_code)
+    receive_thread = threading.Thread(target=getMessage, args=(clientsocket,))
+    receive_thread.start()
+    try:
+        while True:
+            message = input()
+            if (re.match("^send .*\.\w*\s*$", message)):
+                try:
+                    files = re.findall("\s+\w.*\.\w*\s*", message)
+                    fileName = files[0].strip()
+                    filesize = os.path.getsize(fileName)
+                    clientsocket.send(f"{SEND_FILE:<{HEADER_LENGTH}}".encode("utf-8"))
+                    fileHeader = f"{files[0]}{SEPARATOR}{filesize}".encode()
+                    clientsocket.send(f"{len(fileHeader):<{HEADER_LENGTH}}".encode('utf-8'))
+                    clientsocket.send(fileHeader)
+                    f = open(fileName, "rb")
+                    bytes_read = f.read(filesize)
+                    clientsocket.sendall(bytes_read)
+                    f.close()
+                    clientsocket.send(UID)
+                except:
+                    print(f'Not can find {fileName}')
+
+            else:
+                if message:
+                    message_code = message.encode('utf-8')
+                    message_header = f"{len(message_code):<{HEADER_LENGTH}}".encode(
+                        'utf-8')
+                    clientsocket.send(
+                        message_header + message_code )
+    except:
+        os._exit(0)
+
+
+def getMessage(clientsocket):
+    while True:
+        nickname_header = clientsocket.recv(HEADER_LENGTH)
+        current_time = datetime.now().strftime("%H:%M")
+        if len(nickname_header) == 0:
+            print("Close connect")
+            clientsocket.shutdown(socket.SHUT_WR)
+            clientsocket.close()
+            os._exit(0)
+        if nickname_header.decode().strip() == SEND_FILE:
+            name_header = clientsocket.recv(HEADER_LENGTH)
+            name_length = int(name_header.decode())
+            name = clientsocket.recv(name_length).decode()
+            file_header_len = int(clientsocket.recv(HEADER_LENGTH).decode())
+            file_header = clientsocket.recv(file_header_len)
+            filename, filesize = file_header.decode('utf-8').split(SEPARATOR)
+            filename = os.path.basename(filename)
+            filesize = int(filesize)
+            f = open(filename, "wb")
+            total_bytes = bytes()
+            while not UID in total_bytes:
+                bytes_read = clientsocket.recv(filesize)
+                total_bytes+=bytes_read
+            f.write(total_bytes[:(len(total_bytes)-len(UID))])
+            f.close()
+            print(f'<{current_time}> {name} send file {filename}')
+        else:
+            type = nickname_header.decode('utf-8').strip()
+            if type == CONNECT or type == DISCONNECT:
+                warning_length = int(
+                    clientsocket.recv(HEADER_LENGTH).decode('utf-8').strip())
+                warning = clientsocket.recv(warning_length).decode('utf-8')
+                print(f'{warning}')
+                continue
+            nickname_length = int(nickname_header.decode('utf-8').strip())
+            nickname = clientsocket.recv(nickname_length).decode('utf-8')
+            message_header = clientsocket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = clientsocket.recv(message_length).decode('utf-8')
+            print(f'<{current_time}> [{nickname}]: {message}')
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ Server/server.py
@@ -0,0 +1,132 @@
+#coding=utf-8
+import socket
+from datetime import datetime
+import os
+import select
+import time
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 10000
+clients = {}
+SEND_FILE = "SEND_FILE"
+SEPARATOR = "<SEPARATOR>"
+CONNECT = "CONNECT"
+DISCONNECT = "DISCONNECT"
+UID = "c97ec0d1-df22-41f4-858f-7beee9e1bbc4".encode("utf-8")
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen(5)
+    server.setblocking(False)
+    print("Server started!!!")
+    inputs = [server]
+    try:
+        while True:
+            reads, send, excepts = select.select(inputs, [], inputs)
+            for conn in reads:
+                if conn == server:
+                    new_conn, client_addr = conn.accept()
+                    new_conn.setblocking(False)
+                    inputs.append(new_conn)
+                else:
+                    current_time = datetime.now().strftime("%H:%M")
+                    if conn not in clients.keys():
+                        while True:
+                            header = conn.recv(HEADER_LENGTH)
+                            header_len = int(header.decode('utf-8').strip())
+                            name = conn.recv(header_len).decode("utf-8")
+                            if name in clients.values():
+                                code_n = f'{DISCONNECT:<{HEADER_LENGTH}}'.encode('utf-8')
+                                notice = f"Sorry, already have a client with this name: {name} " \
+                                         f"Need choose other name".encode('utf-8')
+                                notice_header = f"{len(notice):<{HEADER_LENGTH}}".encode('utf-8')
+                                message = code_n + notice_header + notice
+                                conn.send(message)
+                                conn.close()
+                            else:
+                                clients[conn] = name
+                                print(f"In {current_time} connected new client - {name}")
+                                notificationForClient(CONNECT, conn)
+                            break
+                    else:
+                        while True:
+                            try:
+                                header = conn.recv(HEADER_LENGTH)
+                                name = clients[conn].encode("utf-8")
+                                if header.decode('utf-8').strip() != SEND_FILE:
+                                    header_name = f'{len(name):<{HEADER_LENGTH}}'.encode('utf-8')
+                                    message = conn.recv(int(header))
+                                    print(
+                                        f'in {current_time}  recieved message from {clients[conn]}: {message.decode("utf-8")}')
+                                    final_message = header_name + name + header + message
+                                    sendToAll(final_message, conn)
+                                else:
+                                    file_header_len_header = conn.recv(HEADER_LENGTH).decode("utf-8")
+                                    file_header_len = int(file_header_len_header)
+                                    file_header = conn.recv(file_header_len)
+                                    filename, filesize = file_header.decode('utf-8').split(
+                                        SEPARATOR)
+                                    filesize = int(filesize)
+                                    total_bytes = bytes()
+                                    time.sleep(1)
+                                    try:
+                                        while not UID in total_bytes:
+                                            bytes_read = conn.recv(filesize)
+                                            total_bytes += bytes_read
+                                    finally:
+                                        file_header_len = f'{len(file_header):<{HEADER_LENGTH}}'.encode('utf-8')
+                                        name_header =  f'{len(name):<{HEADER_LENGTH}}'.encode('utf-8')
+                                        sendFileFlag = f'{SEND_FILE:<{HEADER_LENGTH}}'.encode('utf-8')
+                                        fileMessage = sendFileFlag + name_header + name + file_header_len + file_header + total_bytes
+                                        print(
+                                            f"In {current_time} client {clients[conn]} send file {filename}")
+                                        sendToAll(fileMessage, conn)
+                            except:
+                                inputs.remove(conn)
+                                print(f"{clients[conn]} disconnected")
+                                notificationForClient(DISCONNECT, conn)
+                                del clients[conn]
+                                conn.close()
+                                break
+
+                            break
+
+            for conn in excepts:
+                print(f"{clients[conn]} disconnected")
+                notificationForClient(DISCONNECT, conn)
+                inputs.remove(conn)
+                conn.close()
+                del clients[conn]
+
+    except KeyboardInterrupt:
+        for cl in clients:
+            cl.shutdown(socket.SHUT_WR)
+            cl.close()
+        server.shutdown(socket.SHUT_WR)
+        server.close()
+        os._exit(0)
+
+
+def sendToAll(msg, clientsocket):
+    for client in clients:
+        if client != clientsocket:
+            client.send(msg)
+
+
+def notificationForClient(type, clientsocket):
+    code_n = f'{type:<{HEADER_LENGTH}}'.encode('utf-8')
+    notice: bytes
+    name = clients[clientsocket]
+    if type == CONNECT:
+        notice = f"{name} join chat ".encode('utf-8')
+    if type == DISCONNECT:
+        notice = f"{name} closed chat ".encode('utf-8')
+    notice_header = f"{len(notice):<{HEADER_LENGTH}}".encode('utf-8')
+    message = code_n + notice_header + notice
+    sendToAll(message, clientsocket)
+
+if __name__ == '__main__':
+    main()
