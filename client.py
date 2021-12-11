7d6cdc15b66bfdba5274d19c20b8e14a603b52e1
TsaplinIA <tsapli.ia@edu.spbstu.ru>
2021-12-11 11:54:06 +0300

    Lab4_1

--- client.py
+++ client.py
@@ -1,76 +1,83 @@
-import sys, re, requests
-
+# python_socketio==4.6.0; python_engineio==3.13.2; flask_socketio==4.3.1
+from socketIO_client import SocketIO
+import sys, re
 
 def parse(string):
-    string = string.replace(" ", "")
-    if re.fullmatch("([0-9]+[.|])?[0-9]+\+([0-9]+[.|])?[0-9]+", string):
-        operator = "+"
-        operands = string.split("+")
-    elif re.fullmatch("([0-9]+[.|])?[0-9]+\-([0-9]+[.|])?[0-9]+", string):
-        operator = "-"
-        operands = string.split("-")
-    elif re.fullmatch("([0-9]+[.|])?[0-9]+\*([0-9]+[.|])?[0-9]+", string):
-        operator = "*"
-        operands = string.split("*")
-    elif re.fullmatch("([0-9]+[.|])?[0-9]+\/([0-9]+[.|])?[0-9]+", string):
-        operator = "/"
-        operands = string.split("/")
-    elif re.fullmatch("sqrt\(([0-9]+[.|])?[0-9]+\)", string):
-        operator = "sqrt"
-        operands = (string[5:-1], None)
-    elif re.fullmatch("\d+!", string):
-        operator = "!"
-        operands = (string[:-1], None)
-    else:
-        return None, None, None
+	string = string.replace(" ", "")
+	if re.fullmatch("(-|)([0-9]+[.|])?[0-9]+\+([0-9]+[.|])?[0-9]+", string):
+		operator = "+"
+		operands = string.split("+")
+	elif re.fullmatch("([0-9]+[.|])?[0-9]+\-([0-9]+[.|])?[0-9]+", string):
+		operator = "-"
+		operands = string.split("-")
+	elif re.fullmatch("([0-9]+[.|])?[0-9]+\*([0-9]+[.|])?[0-9]+", string):
+		operator = "*"
+		operands = string.split("*")
+	elif re.fullmatch("([0-9]+[.|])?[0-9]+\/([0-9]+[.|])?[0-9]+", string):
+		operator = "/"
+		operands = string.split("/")
+	elif re.fullmatch("sqrt\(([0-9]+[.|])?[0-9]+\)", string):
+		operator = "sqrt"
+		operands = (string[5:-1], None)
+	elif re.fullmatch("\d+!", string):
+		operator = "!"
+		operands = (string[:-1], None)
+	else:
+		return None, None, None
 
-    return operator, operands[0], operands[1]
+	return operator, operands[0], operands[1]
 
+if len(sys.argv) != 4:
+	print("Args: server login password")
+	exit()
 
-if len(sys.argv) == 4:
-    server = "http://" + sys.argv[1] + ":5000/"
-    login = sys.argv[2]
-    password = sys.argv[3]
-    try:
-        # r = requests.get("http://192.168.0.104:5000/", auth = (login, password))
-        r = requests.get(server, auth=(login, password))
-    except requests.exceptions.RequestException:
-        print("The server is unavailable")
-        exit()
+sio = SocketIO("http://" + sys.argv[1], 5000)
 
-    if r.text != "Hello. You are logged in.":
-        print("Invalid login or password")
-        exit()
-else:
-    print("Args: login password")
-    exit()
+def show_callback_result(*args):
+	print(*args)
+	print()
+sio.on("server_message", show_callback_result)
 
-print("Fast operations: 1+2, 2-3, 3*4, 4/5")
-print("Slow operations: sqrt(25), 5!")
-print("Enter \q to quit", end="\n\n")
+# Step 3 = make requests for calculations and get the results
+def input_handler():
+	while True:
+		string = input()
+		if string == "\q": break
+		operation, operand1, operand2 = parse(string)
+		if operation == None:
+			print("Wrong operation")
+			continue
+	
+		if operation in ("+", "-", "*", "/"):
+			sio.emit(	'fast_calc', 
+						{"operation":operation, "operand1":operand1, "operand2":operand2},
+						callback = show_callback_result
+			)
+		if operation in ("sqrt", "!"):
+			sio.emit(	'slow_calc',
+						{"operation":operation, "value":operand1},
+						callback = show_callback_result
+			)
+		sio.wait_for_callbacks()
 
-fast_operations = ["+", "-", "*", "/"]
-slow_operations = ["sqrt", "!"]
-server += "calc"
-while True:
-    string = input("Enter the expression: ")
-    if string == "\q": break
-    operation, operand1, operand2 = parse(string)
-    if operation is None:
-        print("Wrong operation")
-        continue
+# Step 2 = successful / failed login
+def login_status(*data):
+	if data[0]["status"] != "success":
+		print("Invalid login or password")
+	else:
+		print("Logged in as", data[0]["user_login"])
+		print("Fast operations: 1+2, 2-3, 3*4, 4/5")
+		print("Slow operations: sqrt(25), 5!")
+		print("Enter \q to quit")
+		print()
+		input_handler()
 
-    try:
-        if operation in fast_operations:
-            r = requests.get(server,
-                             params={"operation": operation, "operand1": operand1, "operand2": operand2},
-                             auth=(login, password))
-        else:
-            r = requests.get(server,
-                             params={"operation": operation, "value": operand1},
-                             auth=(login, password))
-    except requests.exceptions.RequestException:
-        print("The server is unavailable")
-        break
+# Step 1 = try to login
+sio.emit(	"login",
+			{'login':sys.argv[2], 'password':sys.argv[3]},
+			callback = login_status
+)
+sio.wait_for_callbacks()
 
-    print("Answer: " + r.text, end="\n\n")
+# before exit
+sio.disconnect()
