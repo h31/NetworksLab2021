6edde7317436cf8f2cc12d46d7fce2d3a0a58d04
unknown <xboooox123@gmail.com>
2021-12-12 03:17:04 +0300

    lab4_1

--- calc/server.py
+++ calc/server.py
@@ -42,9 +42,9 @@ def login(data):
 			flask_login.login_user(user)
 			
 	if current_user.is_authenticated:
-		return {"status":"success", "user_login":current_user.get_id()}
+		return {"status":"success", "user_login":current_user.get_id()}, 200
 	else:
-		return {"status":"failed"}
+		return {"status":"failed"}, 401
 
 def authenticated_only(f):
 	@functools.wraps(f)
@@ -59,60 +59,63 @@ def authenticated_only(f):
 @authenticated_only
 def fast_calc(data):	
 	if data["operation"] not in ("+", "-", "*", "/"):
-		return "ERROR: Wrong operation"
+		return {"ERROR":"Wrong operation"}, 400
 	try:
 		operand1 = float(data["operand1"])
 		operand2 = float(data["operand2"])
 	except ValueError or TypeError:
-		return "ERROR: Invalid operand"
+		return {"ERROR":"Invalid operand"}, 400
 	
 	match data["operation"]:
 		case "+":
-			return "Answer: " + str(operand1) + " + " + str(operand2) + " = " + str(operand1 + operand2)
+			return {"Answer":str(operand1) + " + " + str(operand2) + " = " + str(operand1 + operand2)}, 200
 		case "-":
-			return "Answer: " + str(operand1) + " - " + str(operand2) + " = " + str(operand1 - operand2)
+			return {"Answer":str(operand1) + " - " + str(operand2) + " = " + str(operand1 - operand2)}, 200
 		case "*":
-			return "Answer: " + str(operand1) + " * " + str(operand2) + " = " + str(operand1 * operand2)
+			return {"Answer":str(operand1) + " * " + str(operand2) + " = " + str(operand1 * operand2)}, 200
 		case "/":
 			if operand2 > -0.0000001 and operand2 < 0.0000001:
-				return "ERROR: Division by zero"
+				return {"ERROR":"Division by zero"}, 400
 			else:
-				return "Answer: " + str(operand1) + " / " + str(operand2) + " = " + str(operand1 / operand2)
+				return {"Answer":str(operand1) + " / " + str(operand2) + " = " + str(operand1 / operand2)}, 200
 
 @socketio.on('slow_calc')
 @authenticated_only
 def slow_calc(data):
 	if data["operation"] not in ("sqrt", "!"):
-		return "ERROR: Wrong operation"
+		return {"ERROR":"Wrong operation"}, 400
 	try:
 		value = float(data["value"])
 	except ValueError or TypeError:
-		return "ERROR: Invalid value"
+		return {"ERROR":"Invalid value"}, 400
 	
-	emit("server_message", "INFO: Please wait until the operation is completed")
+	emit("server_message", ({"INFO":"Please wait until the operation is completed"}, 200))
 	
+	# https://docs.python.org/3/library/multiprocessing.html
+	# https://docs-python.ru/standart-library/paket-multiprocessing-python/klass-pool-modulja-multiprocessing/
+	# https://docs-python.ru/standart-library/paket-multiprocessing-python/funktsija-process-modulja-multiprocessing/
 	pool = Pool()
 	asyncResult = pool.apply_async(calculate, (data["operation"], value))
-	asyncResult.wait(int(sys.argv[1]))
+	asyncResult.wait(int(sys.argv[1])) # waiting for a timeout or until the result is available
 	if asyncResult.ready():
 		result = asyncResult.get()
 		pool.terminate()
 		return result
 	else:
 		pool.terminate()
-		return "ERROR: Timeout"
+		return {"ERROR":"Timeout"}, 400
 
 def calculate(operation, value):
-	if value < 0: return "ERROR: The value must be greater than 0"
+	if value < 0: return {"ERROR":"The value must be greater than 0"}, 400
 	match operation:
 		case "sqrt":
-			return "Answer: sqrt(" + str(value) + ") = " + str(math.sqrt(value))
+			return {"Answer":"sqrt(" + str(value) + ") = " + str(math.sqrt(value))}, 200
 		case "!":
 			try:
 				factorial = math.factorial(int(value))
-				return "Answer: " + str(int(value)) + "! = " + str(factorial)
+				return {"Answer":str(int(value)) + "! = " + str(factorial)}, 200
 			except OverflowError:
-				return "ERROR: The factorial argument should not exceed 2147483647"
+				return {"ERROR":"The factorial argument should not exceed 2147483647"}, 400
 
 def get_local_IP():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
