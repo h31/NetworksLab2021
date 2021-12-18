e1038af67efed8752fc2ab4b4529a69c62a0250d
killawetz <vasiliev2.rv@edu.spbstu.ru>
2021-12-18 14:39:26 +0300

    added log4j

--- Lab4/server/src/main/java/com/example/delservice/controller/OrderController.java
+++ Lab4/server/src/main/java/com/example/delservice/controller/OrderController.java
@@ -2,22 +2,24 @@ package com.example.delservice.controller;
 
 import com.example.delservice.dto.OrderDTO;
 import com.example.delservice.dto.OrderPriceDTO;
-import com.example.delservice.model.Market;
 import com.example.delservice.service.MarketService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiOperation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Map;
 
 
 @RestController
 public class OrderController {
+
+    private static final Logger logger = LogManager.getLogger(OrderController.class);
+
 
     @Autowired
     private MarketService marketService;
@@ -35,17 +37,12 @@ public class OrderController {
         try {
             userOrder = objectMapper.readValue(json, OrderDTO.class);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error("Threw a JsonProcessingException in OrderController::order," +
+                    " full stack trace follows:", e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect data");
         }
 
-        OrderPriceDTO result = marketService.calculateTheOrder(userOrder);
-
-        if (result == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect data");
-        }
-
-        return result;
+        return marketService.calculateTheOrder(userOrder);
     }
 
 
