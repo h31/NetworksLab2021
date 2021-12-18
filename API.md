cfaf676948b75af0007d661b3285ea38685ef9b6
TsaplinIA <tsapli.ia@edu.spbstu.ru>
2021-12-18 18:42:50 +0300

    some fix

--- API.md
+++ API.md
@@ -1,4 +1,12 @@
 # Roulette API
+## Errors
+ERROR 435 - The croupier already exists
+ERROR 441 - try login twice
+ERROR 442 - Some field is not filled
+ERROR 443 - Incorrect login or password
+ERROR 444 - Incorrect type or amount
+ERROR 445 - insufficient number of coins
+
 ## Routes
 - POST /login
   - WWW-FORM{"login": "String", "password": "String", "croupier": "String"}
@@ -15,32 +23,34 @@
   - Ответы:
     - Code:200
       - text/html
-- GET /protected
+- GET /userInfo
   - Ответы:
     - Code:200
       - text/html
     - Code:401
       - Unauthorized
-- GET /protected/json
+- GET /userInfo/json
   - Ответы:
     - Code:200
       - JSON{"coins": "Int", "is_croupier": "boolean", "login": "String"}
     - Code:401
       - Unauthorized
 - GET /logout
-  - Ответы
+  - Ответы:
     - Code:200
       - html/text
     - Code:401
       - Unauthorized
 - GET /bet
-  - Ответы
+  - Ответы:
     - Code: 200
       - html/text
     - Code: 401
       - Unauthorized
+    - Code: 403
+      - You are croupier. You can't do bets
 - POST /bet
-  - Ответы
+  - Ответы:
     - Code: 200
       - bet accepted
     - Code: 401
@@ -52,11 +62,17 @@
     - Code: 445
       - Insufficient number of coins
 - GET /bet/all
-  - Ответы
+  - Ответы:
     - Code: 200
       - html/text
+- GET /bet/all/json
+  - Ответы:
+    - Code: 200
+      - JSON:{"users": list<String>,"types": list<Int>,"amounts": list<Int>}
+    - Code: 204
+      - Bets not found
 - GET /start
-  - Ответы
+  - Ответы:
     - Code: 200
       - html/text
     - Code: 401
@@ -64,6 +80,12 @@
     - Code: 403
       - You must be croupier
 - GET /result
-  - Ответы
+  - Ответы:
     - Code: 200
       - html/text
+- GET /result/all
+  - Ответы:
+    - Code: 204
+      - No results yet
+    - Code: 200
+      - JSON:{"number": Int, "users": list<String>,"types": list<Int>,"amounts": list<Int>, "result": list<String>}
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import random
-import flask_login
+import flask_login  # from requirements.txt
 import flask
 
 from flask import jsonify, make_response
@@ -12,7 +12,7 @@ app.secret_key = 'qwerty'
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
-addr = '192.168.0.102'
+addr = '0.0.0.0'
 
 users = {
     'Tsaplin': [{'password': '12345'}, 1200],
@@ -32,75 +32,62 @@ def user_loader(login):
 
 @login_manager.request_loader
 def request_loader(request):
-    login = request.form.get('login')
-    if login not in logged_in_users:
+    user_login = request.form.get('login')
+    if user_login not in logged_in_users:
         return
-
-    try:
-        is_croupier = request.form.get('croupier') == 'on'
-    except:
-        is_croupier = False
-
-    user = User(users[login][1], is_croupier)
-    user.id = login
-
-    return user
+    return logged_in_users[loginProcess]
 
 
 @app.route('/')
 def redirect():
     if flask_login.current_user.is_authenticated:
-        return flask.redirect(flask.url_for('protected'))
+        return flask.redirect(flask.url_for('userInfo'))
     return flask.redirect(flask.url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
-    if flask.request.method == 'GET':
-        if flask_login.current_user.is_authenticated:
-            print(flask_login.current_user.id)
-            return flask.redirect(flask.url_for('protected'))
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='login' id='login' placeholder='login'/>
-                <input type='password' name='password' id='password' placeholder='password'/>
-                <input type='checkbox' name='croupier' id='croupier' placeholder='croupier'/>
-                Login as croupier
-                <input type='submit' name='submit'/>
-               </form>
-               '''
-
-    if flask_login.current_user.is_authenticated:
-        return "You already login", 441  # ERROR 441 - try login twice
-    try:
-        local_login = flask.request.form['login']
-        password = flask.request.form['password']
-    except:
-        return "You must specify the login and password", 442  # ERROR 442 - Some field is not filled
-
-    if local_login not in users:
-        return bad_login(443)  # ERROR 443 - Incorrect login or password
-
-    try:
-        is_croupier = flask.request.form['croupier'] == 'on'
-    except:
-        is_croupier = False
-
-    if password == users[local_login][0]['password']:
-        user = User(users[local_login][1], is_croupier)
-        user.id = local_login
-
-        global croupier
-        if is_croupier:
-            if croupier is None:
-                croupier = user
+def loginProcess():
+    match flask.request.method:
+        case 'GET':
+            if flask_login.current_user.is_authenticated:
+                return flask.redirect(flask.url_for('userInfo'))
+            return '''
+                   <form action='login' method='POST'>
+                    <input type='text' name='login' id='login' placeholder='login'/>
+                    <input type='password' name='password' id='password' placeholder='password'/>
+                    <input type='checkbox' name='croupier' id='croupier' placeholder='croupier'/>
+                    Login as croupier
+                    <input type='submit' name='submit'/>
+                   </form>
+                   ''', 200
+        case _:
+            if flask_login.current_user.is_authenticated:
+                return "You already login", 441  # ERROR 441 - try login twice
+            form = flask.request.form
+            if 'login' not in form or 'password' not in form:
+                return "You must specify the login and password", 442  # ERROR 442 - Some field is not filled
+            user_login = form['login']
+            user_password = form['password']
+            if user_login not in users:
+                return bad_login(443)
+            if 'croupier' in form:
+                is_croupier = form['croupier'] == 'on'
             else:
-                return 'the croupier already exists', 435
+                is_croupier = False
+            if user_password == users[user_login][0]['password']:
+                user = User(users[user_login][1], is_croupier)
+                user.id = user_login
 
-        logged_in_users[local_login] = user
-        flask_login.login_user(user)
-        return flask.redirect(flask.url_for('protected'))
+                global croupier
+                if croupier is not None and is_croupier:
+                    return 'the croupier already exists', 435
 
+                if is_croupier:
+                    croupier = user
+
+                logged_in_users[user_login] = user
+                flask_login.login_user(user)
+                return flask.redirect(flask.url_for('userInfo'))
     return bad_login(443)
 
 
@@ -113,23 +100,23 @@ def bad_login(code):
     ''', code
 
 
-@app.route('/protected')
+@app.route('/userInfo')
 @flask_login.login_required
-def protected():
+def userInfo():
     doc = f'''
         <br>
         Logged in as: {flask_login.current_user.id} \n
         </br><br>
         Your coins: {flask_login.current_user.coins}
         </br>
-    '''
+    ''', 200
 
     return doc
 
 
-@app.route('/protected/json')
+@app.route('/userInfo/json')
 @flask_login.login_required
-def protected_json():
+def userInfo_json():
     data = {
         'login': flask_login.current_user.id,
         'coins': flask_login.current_user.coins,
@@ -144,11 +131,9 @@ def logout():
     global croupier
     if flask_login.current_user.is_croupier:
         croupier = None
-
-    logged_in_users.pop(flask_login.current_user.id)
-
+    del logged_in_users[flask_login.current_user.id]
     flask_login.logout_user()
-    return 'Logged out'
+    return 'Logged out', 200
 
 
 @app.route('/bet', methods=['GET', 'POST'])
@@ -156,40 +141,59 @@ def logout():
 def bet():
     if flask_login.current_user.is_croupier:
         return "You are croupier. You can't do bets", 403
-    if flask.request.method == 'GET':
-        return f'''
-               <form action='bet' method='POST'>
-                Type:<input type='number' min='-2' max='36' name='type' id='type' placeholder='type'/>
-                Bet amount:<input type='number' min='1' name='amount' id='amount' placeholder='amount'/>
-                <input type='submit' name='submit'/>
-               </form>
-               '''
-
-    try:
-        amount = int(flask.request.form['amount'])
-        type = int(flask.request.form['type'])
-    except:
-        return "you must specify the correct type and amount", 444
-
-    if flask_login.current_user.coins < amount:
-        return "insufficient number of coins", 445
-
-    try:
-        new_bet = Bet(flask_login.current_user.id, amount, type)
-    except:
-        return "impossible bet", 444
-
-    flask_login.current_user.coins -= amount
-    bets.append(new_bet)
-    return "bet accepted"
+    match flask.request.method:
+        case 'GET':
+            return f'''
+                           <form action='bet' method='POST'>
+                            Type:<input type='number' min='-2' max='36' name='type' id='type' placeholder='type'/>
+                            Bet amount:<input type='number' min='1' name='amount' id='amount' placeholder='amount'/>
+                            <input type='submit' name='submit'/>
+                           </form>
+                           ''', 200
+        case _:
+            form = flask.request.form
+            if 'amount' not in form or 'type' not in form:
+                return "you must specify the correct type and amount", 444
+            amount = int(form['amount'])
+            bet_type = int(form['type'])
+            if flask_login.current_user.coins < amount:
+                return "insufficient number of coins", 445
+            try:
+                new_bet = Bet(flask_login.current_user.id, amount, bet_type)
+            except:
+                return "impossible bet", 444
+            flask_login.current_user.coins -= amount
+            bets.append(new_bet)
+            return "bet accepted", 200
 
 
 @app.route('/bet/all', methods=['GET'])
 def bet_all():
     if len(bets) > 0:
-        all = "\n".join(f'''<br>{str(bet)}</br>''' for bet in bets)
-        return all
-    return "Bets not found"
+        all = "\n".join(f"<br>{str(bet)}</br>" for bet in bets)
+        return all, 200
+    return "Bets not found", 204
+
+
+@app.route('/bet/all/json', methods=['GET'])
+def bet_all_json():
+    all_users = []
+    all_types = []
+    all_amounts = []
+    if len(bets) > 0:
+        for bet_i in bets:
+            all_users.append(bet_i.user_id)
+            all_types.append(bet_i.bet_type)
+            all_amounts.append(bet_i.amount)
+
+        data = {
+            "users": all_users,
+            "types": all_types,
+            "amounts": all_amounts
+        }
+
+        return make_response(jsonify(data), 200)
+    return "Bets not found", 204  # No content
 
 
 @app.route('/start', methods=['GET'])
@@ -201,32 +205,59 @@ def start():
     local_result = ""
     number = getRandomNum()
     local_result += f'The resulting number: {number}'
+    all_users = []
+    all_types = []
+    all_amounts = []
+    all_res = []
     for el in bets:
+        all_users.append(el.user_id)
+        all_types.append(el.bet_type)
+        all_amounts.append(el.amount)
         if el.getResult(number):
             win_str = "WIN"
+            all_res.append("WIN")
         else:
             win_str = "LOSE"
+            all_res.append("LOSE")
         bet_str = str(el)
         local_result += f'\n<br>{bet_str} -- {win_str}</br>'
 
         prize = el.getPrize(number)
-        try:
+
+        if el.user_id in logged_in_users:
             logged_in_users[el.user_id].coins += prize
-        except:
+        else:
             users[el.user_id][1] += prize
 
     bets = []
     result = local_result
+    global result_dict
+    result_dict = {
+        "number": number,
+        "users": all_users,
+        "types": all_types,
+        "amounts": all_amounts,
+        "result": all_res
+    }
     return f'Successful. Number: {number}.', 200
 
 
 @app.route('/result', methods=['GET'])
 def results():
-    return result
+    return result, 200
+
+
+@app.route('/result/json', methods=['GET'])
+def results_json():
+    if len(result_dict) == 0:
+        return "No results yet", 204
+    else:
+        return make_response(jsonify(result_dict), 200)
 
 
 bets = []
 result = "No results yet"
+result_dict = dict()
 croupier = None
 logged_in_users = dict()
 
@@ -237,7 +268,3 @@ def getRandomNum():
 
 if __name__ == "__main__":
     app.run(host=addr, port=5000)
-
-# number = getRandomNum()
-# for bet in bets:
-#     print(bet.getPrize(number))
