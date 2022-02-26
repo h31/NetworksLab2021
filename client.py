2ab9cd7141f31751a4e29799179c915ca42bffd2
Shadow-Haund <43119772+Shadow-Haund@users.noreply.github.com>
2022-02-26 16:49:54 +0300

    Внес все исправления в код

--- client.py
+++ client.py
@@ -1,9 +1,6 @@
-import threading
 import time
 import requests, re
 
-user_is_login = False
-regex = '[0-9]+[+\-\/*!^]{1}[0-9]*'
 reg = "(?P<op1>[0-9]+)(?P<oper>[+\-\/*!^]?)(?P<op2>[0-9]*)"
 hostF = "http://localhost:8080/fast"
 hostS = "http://localhost:8080/slow"
@@ -11,10 +8,28 @@ hostS = "http://localhost:8080/slow"
 user_name = input("Имя пользователя ")
 password = input("Пароль ")
 
+fast_oper = {'+': "add", '-': "sub", '/': "div", '*': "mul"}
+slow_oper = {'^': "pow", '!': "fact"}
 
-def sending(host, json, path, name, passw):
+
+def send(host, json, path, name, passw):
     response = requests.post(f"{host}/{path}", json=json, auth=(name, passw))
-    print(response.json()["rez"])
+    print(response.json()["res"])
+
+
+def send_long(host, json, path, name, passw):
+    response = requests.post(f"{host}/{path}", json=json, auth=(name, passw))
+    response_one = response.json()
+    print(f"Задача обрабатывается, для проверки готовности используй номер задачи {response_one['token']}")
+    json_to_send_two = {"token": int(input("Номер задачи "))}
+    while True:
+        response_two = requests.post(f"{host}/{path}", json=json_to_send_two, auth=(name, passw))
+        if response_two.json()["status"]:
+            print(response_two.json()["res"])
+            break
+        else:
+            print("Операция выполняется")
+            time.sleep(2)
 
 
 while True:
@@ -26,39 +41,9 @@ while True:
         json_to_send = {"op1": int(reGroup.group('op1')),
                         "op2": int(reGroup.group('op2'))}
     if reGroup is not None:
-        if "+" == reGroup.group('oper'):
-            sending(hostF, json_to_send, "add", user_name, password)
-        if "-" in operation_sep:
-            sending(hostF, json_to_send, "sub", user_name, password)
-        if "/" in operation_sep:
-            sending(hostF, json_to_send, "div", user_name, password)
-        if "*" in operation_sep:
-            sending(hostF, json_to_send, "mul", user_name, password)
-        if "!" in operation_sep:
-            response = requests.post(f"{hostS}/fact", json=json_to_send, auth=(user_name, password))
-            response_one = response.json()
-            print(f"Задача обрабатывается, для проверки готовности используй номер задачи {response_one['token']}")
-            json_to_send_two = {"token": int(input("Номер задачи "))}
-            while True:
-                response_two = requests.post(f"{hostS}/fact", json=json_to_send_two, auth=(user_name, password))
-                if response_two.json()["status"] == 'ready':
-                    print(response_two.json()["res"])
-                    break
-                else:
-                    print(response_two.json()["status"])
-                    time.sleep(2)
-        if "^" in operation_sep:
-            response = requests.post(f"{hostS}/pow", json=json_to_send, auth=(user_name, password))
-            response_one = response.json()
-            print(f"Задача обрабатывается, для проверки готовности используй номер задачи {response_one['token']}")
-            json_to_send_two = {"token": int(input("Номер задачи "))}
-            while True:
-                response_two = requests.post(f"{hostS}/pow", json=json_to_send_two, auth=(user_name, password))
-                if response_two.json()["status"] == 'ready':
-                    print(response_two.json()["res"])
-                    break
-                else:
-                    print(response_two.json()["status"])
-                    time.sleep(2)
+        if reGroup.group('oper') in fast_oper:
+            send(hostF, json_to_send, fast_oper[reGroup.group('oper')], user_name, password)
+        else:
+            send_long(hostS, json_to_send, slow_oper[reGroup.group('oper')], user_name, password)
     else:
         print("Операция введена неверно, введите еще раз")
--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def pow_processing(req, token):
     time.sleep(10)
     global dict_of_res, number
     res = math.pow(req['op1'], req['op2'])
-    dict_of_res[token] = {"status": "ready", "res": res}
+    dict_of_res[token] = {"status": True, "res": res}
     print(f"for power with token {token} result is {res}")
 
 
@@ -28,82 +28,53 @@ def fact_processing(req, token):
     time.sleep(10)
     global dict_of_res, number
     res = math.factorial(req['op1'])
-    dict_of_res[token] = {"status": "ready", "res": res}
+    dict_of_res[token] = {"status": True, "res": res}
     print(f"for factorial with token {token} result is {res}")
 
 
-@route('/fast/add', method=['GET', 'POST'])
-@auth_basic(check_pass)
-def addition():
-    posted = json.loads(request.body.getvalue().decode("utf-8"))
-    rez = posted["op1"] + posted["op2"]
-    posted["rez"] = rez
-    print(rez)
-    return posted
-
-
-@route('/fast/sub', method=['GET', 'POST'])
-@auth_basic(check_pass)
-def subtraction():
-    posted = json.loads(request.body.getvalue().decode("utf-8"))
-    rez = posted["op1"] - posted["op2"]
-    posted["rez"] = rez
-    print(rez)
-    return posted
-
-
-@route('/fast/div', method=['GET', 'POST'])
-@auth_basic(check_pass)
-def division():
-    posted = json.loads(request.body.getvalue().decode("utf-8"))
-    rez = posted["op1"] / posted["op2"]
-    posted["rez"] = rez
-    print(rez)
-    return posted
-
-
-@route('/fast/mul', method=['GET', 'POST'])
-@auth_basic(check_pass)
-def multiplication():
-    posted = json.loads(request.body.getvalue().decode("utf-8"))
-    rez = posted["op1"] * posted["op2"]
-    posted["rez"] = rez
-    print(rez)
-    return posted
-
-
-@route('/slow/fact', method=['GET', 'POST'])
-@auth_basic(check_pass)
-def factorial():
+def slow(oper):
     global number, dict_of_res
-    posted = json.loads(request.body.getvalue().decode("utf-8"))
+    posted = request.json
     if 'token' not in posted.keys():
-        number = number + 1
-        dict_of_res[number-1] = {"status": "work is started"}
-        fact_th = threading.Thread(target=fact_processing, args=(posted, number - 1))
-        fact_th.start()
+        number += 1
+        dict_of_res[number - 1] = {"status": False}
+        if oper == 'fact':
+            fact_th = threading.Thread(target=fact_processing, args=(posted, number - 1))
+            fact_th.start()
+        else:
+            pow_th = threading.Thread(target=pow_processing, args=(posted, number - 1))
+            pow_th.start()
         return {'token': number - 1}
     else:
-        if dict_of_res[posted['token']]["status"] != 'ready':
-            dict_of_res[posted['token']] = {"status": "in progress"}
         return dict_of_res[posted['token']]
 
 
-@route('/slow/pow', method=['GET', 'POST'])
-@auth_basic(check_pass)
-def power():
-    global number, dict_of_res
-    posted = json.loads(request.body.getvalue().decode("utf-8"))
-    if 'token' not in posted.keys():
-        number = number + 1
-        dict_of_res[number-1] = {"status": "work is started"}
-        fact_th = threading.Thread(target=pow_processing, args=(posted, number - 1))
-        fact_th.start()
-        return {'token': number - 1}
+def fast(oper):
+    posted = request.json
+    if oper == "add":
+        posted["res"] = posted["op1"] + posted["op2"]
+        return posted
+    elif oper == "sub":
+        posted["res"] = posted["op1"] - posted["op2"]
+        return posted
+    elif oper == 'div':
+        posted["res"] = posted["op1"] / posted["op2"]
+        return posted
     else:
-        if dict_of_res[posted['token']]["status"] != 'ready':
-            dict_of_res[posted['token']] = {"status": "in progress"}
-        return dict_of_res[posted['token']]
+        posted["res"] = posted["op1"] * posted["op2"]
+        return posted
+
+
+@route('/slow/<operation>', method=['POST'])
+@auth_basic(check_pass)
+def slow_operation(operation):
+    return slow(operation)
+
+
+@route('/fast/<operation>', method=['POST'])
+@auth_basic(check_pass)
+def slow_operations(operation):
+    return fast(operation)
 
 
 run(host='localhost', port=8080)
