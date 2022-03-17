96d59a29585f68bfd19300cc60812885ef3ecebf
Resident0014 <43120915+Resident0014@users.noreply.github.com>
2022-03-17 15:53:01 +0300

    all fixes

---
+++ Chat/Chat/src/Coder.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Coder {
+    // { "type": "init", "nickname": "guest1"}
+    // "type=init&nickname=guest1"
+    public String encodeData(Map<String, String> data) {
+        List<String> params = new ArrayList<>();
+        for (Map.Entry<String, String> e : data.entrySet()) {
+            String value = e.getValue();
+            if (value == null) {
+                params.add(e.getKey());
+            }
+            else {
+                value = value.replaceAll("[&\\\\]", "\\\\$0");
+                params.add(e.getKey() + "=" + value);
+            }
+        }
+        return String.join("&", params);
+    }
+
+    public Map<String, String> decodeData(String msg) {
+        Map<String, String> data = new HashMap<>();
+        for (String pair : msg.split("((?<!\\\\)&)|((?<=\\\\\\\\)&)")) {
+            String[] entry = pair.split("=", 2);
+            data.put(
+                    entry[0],
+                    entry.length == 1 ?
+                            null :
+                            entry[1].replaceAll("\\\\\\\\", "\\\\")
+            );
+        }
+        return data;
+    }
+}
