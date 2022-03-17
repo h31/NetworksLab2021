87679354f98e5326b2444c3b74705587f6345ce2
Resident0014 <43120915+Resident0014@users.noreply.github.com>
2022-03-17 18:41:34 +0300

    Delete Chat directory

--- Chat/ChatClient/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.chatclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.chatclient to javafx.fxml;
-    exports com.example.chatclient;
-}
