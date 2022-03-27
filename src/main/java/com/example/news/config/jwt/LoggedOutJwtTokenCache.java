d7b0320bcb5f77a421113c5f1aacb79bd195e436
ilya.trachuk <Ta3fuvcu>
2022-03-28 01:44:30 +0300

    add jwt blacklist

---
+++ src/main/java/com/example/news/config/jwt/LoggedOutJwtTokenCache.java
@@ -0,0 +1,48 @@
+package com.example.news.config.jwt;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.jodah.expiringmap.ExpiringMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
+@Getter
+public class LoggedOutJwtTokenCache {
+
+    private final ExpiringMap<String, String> tokenEventMap;
+    private final JWTUtil jwtUtil;
+
+    @Autowired
+    public LoggedOutJwtTokenCache(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+        this.tokenEventMap = ExpiringMap.builder()
+                .variableExpiration()
+                .maxSize(1000)
+                .build();
+    }
+
+    public void markLogoutEventForToken(String token) {
+        if (tokenEventMap.containsKey(token)) {
+            log.info(String.format("Log out token for user [%s] is already present in the cache", jwtUtil.extractUsername(token)));
+
+        } else {
+            Date tokenExpiryDate = jwtUtil.extractExpiration(token);
+            long ttlForToken = getTTLForToken(tokenExpiryDate);
+            log.info("Logout token cached" + jwtUtil.extractUsername(token), ttlForToken, tokenExpiryDate);
+            tokenEventMap.put(token, jwtUtil.extractUsername(token), ttlForToken, TimeUnit.SECONDS);
+        }
+    }
+
+
+    private long getTTLForToken(Date date) {
+        long secondAtExpiry = date.toInstant().getEpochSecond();
+        long secondAtLogout = Instant.now().getEpochSecond();
+        return Math.max(0, secondAtExpiry - secondAtLogout);
+    }
+}
