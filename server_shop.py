8a9600bbe1a53abfbf2c86dbdbb193dc4b3034b4
copchenka <mandragora2319@mail.ru>
2022-03-28 02:50:22 +0300

    Update server_shop.py

--- server_shop.py
+++ server_shop.py
@@ -5,11 +5,12 @@ from flask import Flask, request, current_app, jsonify
 app = Flask(__name__)
 
 storage = {'users': [], 'products': []}
-
+none = {"name": "", "price": 0}
 
 @app.route('/ping')
 def hello_world():
     return '.'
+
 
 
 @app.route("/users", methods=["GET", "POST"])
@@ -19,7 +20,7 @@ def registration():
     elif request.method == "POST":
         form = json.loads(request.data)
         if "name" not in form or "balance" not in form:
-            return "", 400
+            return jsonify(none)
         storage["users"].append({"name": form["name"], "balance": form["balance"], "goods": []})
         return form
 
@@ -31,7 +32,7 @@ def reg_product():
     elif request.method == "POST":
         form = json.loads(request.data)
         if "name" not in form or "price" not in form:
-            return "", 400
+            return jsonify(none)
         storage["products"].append({"name": form["name"], "price": form["price"]})
         return form
 
@@ -44,7 +45,7 @@ def drop_product(name):
     else:
         product = list(filter(lambda el: el["name"] == name, storage["products"]))
         if len(product) == 0:
-            return "", 400
+            return jsonify(none)
         else:
             return jsonify(product[0])
 
@@ -53,13 +54,13 @@ def drop_product(name):
 def add_product(name, money, good):
     product = list(filter(lambda el: el["name"] == good, storage["products"]))
     if len(product) == 0:
-        return "no product", 400
+        return jsonify(none)
     user = list(filter(lambda el: el["name"] == name, storage["users"]))
     if len(user) == 0:
-        return "no user", 400
+        return jsonify(none)
     money -= product[0]["price"]
     if money < 0:
-        return "not another money", 400
+        return jsonify(none)
     i = next((index for (index, d) in enumerate(storage["users"]) if d["name"] == name), None)
     storage["users"][i]["balance"] = money
     return "."
